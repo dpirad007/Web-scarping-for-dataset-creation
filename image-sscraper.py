3be920a43f963d673634498eb30d1e29f9c00699
Dion Pinto <dionpinto000@gmail.com>
2020-03-13 20:18:16 +0530

    scroll through desired query in google images

--- image-sscraper.py
+++ image-sscraper.py
@@ -6,10 +6,20 @@ from selenium import webdriver
 driver_path=r'C:\Users\Dionp\Desktop\dataset creator\chromedriver.exe'
 
 wd =webdriver.Chrome(executable_path=driver_path)
+a=input("type your query: ")
+url="https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={a}&oq={a}&gs_l=img"
 
-wd.get('https://google.com')
-
-search_box=wd.find_element_by_css_selector('input.gLFyf')
-search_box.send_keys('Dogs')
+wd.get(url.format(a=a))
+time.sleep(5)
+start=0
+#wd.back()
+thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
+#print(len(thumbnail_results))
+for img in thumbnail_results[start:len(thumbnail_results)]:
+    try:
+        img.click()
+        
+    except Exception:
+        continue
 time.sleep(10)
 wd.quit()
