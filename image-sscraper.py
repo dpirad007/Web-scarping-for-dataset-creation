fdf2ae4b7f624d35db0a2937a567f18affd4b013
Dion Pinto <dionpinto000@gmail.com>
2020-03-13 18:05:13 +0530

    extended time for viewing chrome

--- image-sscraper.py
+++ image-sscraper.py
@@ -1,5 +1,15 @@
+#https://towardsdatascience.com/image-scraping-with-python-a96feda8af2d
+
+import time
 from selenium import webdriver
 
 driver_path=r'C:\Users\Dionp\Desktop\dataset creator\chromedriver.exe'
 
 wd =webdriver.Chrome(executable_path=driver_path)
+
+wd.get('https://google.com')
+
+search_box=wd.find_element_by_css_selector('input.gLFyf')
+search_box.send_keys('Dogs')
+time.sleep(10)
+wd.quit()
